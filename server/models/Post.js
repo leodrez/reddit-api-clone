6793e8edbff06802410281a38e63329d82d0c9f0
Leonardo Andres <leonardoandres.1298@gmail.com>
2017-07-28 15:01:36 -0400

    Comment model added. Post comment fixed

--- server/models/Post.js
+++ server/models/Post.js
@@ -22,7 +22,13 @@ const postSchema = new Schema({
   _creator: {
     type: Schema.ObjectId,
     ref: 'User' 
-  }
+  },
+  _comments: [
+    {
+      type: Schema.ObjectId,
+      ref: 'Comment'
+    } 
+  ]
 });
 
 const Post = mongoose.model('Post', postSchema);

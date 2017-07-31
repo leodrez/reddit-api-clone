756355609b795020698c2a1e3e965c0d618122e0
Leonardo Andres <leonardoandres.1298@gmail.com>
2017-07-31 18:33:15 -0400

    Auto populate function commented on Comment model. Function implemented on controller

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -27,5 +27,16 @@ const commentSchema = new Schema({
   }
 });
 
+/*const autoPopulateCreator = function(next) {
+  this.populate({
+    path: '_creator',
+    select: 'username createdAt -_id'
+  });
+  next();
+}
+
+commentSchema.pre('find', autoPopulateCreator);
+*/
+
 const Comment = mongoose.model('Comment', commentSchema);
 export default Comment;

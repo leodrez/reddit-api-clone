978bafe2e54d726af44f537b0963121c1400f25c
Leonardo Andres <leonardoandres.1298@gmail.com>
2017-07-31 19:06:16 -0400

    Comment controller exported and added to routes

--- server/routes.js
+++ server/routes.js
@@ -4,6 +4,7 @@ import express from 'express';
 import rootController from './controllers/rootController';
 import userController from './controllers/userController';
 import postController from './controllers/postController';
+import commentController from './controllers/commentController';
 
 const routes = express();
 
@@ -17,6 +18,8 @@ routes.post('/signup', userController.post);
 routes.post('/post', postController.post);
 routes.get('/posts', postController.getAll);
 
+// Comment routes
+routes.post('/comment', commentController.post);
 
 
 export default routes;
